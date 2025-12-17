1c1139a83b206ee7aab917bf758871ee27998280
yama720 <m.yamada417@gmail.com>
2025-12-17 11:11:37 +0900

    7章まで完了

--- app/_components/SearchField/SearchField.tsx
+++ app/_components/SearchField/SearchField.tsx
@@ -1,11 +1,32 @@
+'use client';
+
 import styles from './SearchField.module.css';
 import Image from 'next/image';
-
+import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
+import { Suspense } from 'react';
 
-export default function SearchField() {
+function SearchFieldComponent() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const q = e.currentTarget.elements.namedItem('q');
+    // qという属性がついていた場合(ここだとinputタグ)、それをqに入れる
+
+    if (q instanceof HTMLInputElement) {
+      const params = new URLSearchParams();
+      // paramsをURLを扱えるようにする
+      params.set('q', q.value.trim());
+      // 要らん値を切る
+      router.push(`/news/search?${params.toString()}`);
+      // toString:上記までに成形してきた文字列をparamsに入れる
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <label className={styles.search}>
         <Image
           src="/search.svg"
@@ -17,10 +38,19 @@ export default function SearchField() {
         <input
           type="text"
           name="q"
+          defaultValue={searchParams.get('q') ?? undefined}
           placeholder="キーワードを入力"
           className={styles.searchInput}
         />
       </label>
     </form>
+  );
+}
+
+export default function SearchField() {
+  return (
+    <Suspense>
+      <SearchFieldComponent />
+    </Suspense>
   );
 }
