f4abddd886622ab2b9a1e7e02e56f1a75a3bc1a1
yama720 <m.yamada417@gmail.com>
2025-12-18 11:04:18 +0900

    下書きレビュー時はSSRにする

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -75,6 +75,11 @@ export const getCategoryDetail = async (
     endpoint: 'categories',
     contentId,
     queries,
+    customRequestInit: {
+      next: {
+        revalidate: queries?.draftKey === undefined ? 60 : 0,
+      },
+    },
   });
   return detailData;
 };
--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { getNewsDetail } from '@/app/_libs/microcms';
 import styles from './page.module.css';
 import ButtonLink from '@/app/_components/ButtonLink';
 import { notFound } from 'next/navigation';
+import SearchField from '@/app/_components/SearchField/SearchField';
 
 type Props = {
   params: {
@@ -26,6 +27,7 @@ export default async function Page({ params, searchParams }: Props) {
 
   return (
     <>
+      <SearchField />
       <Article data={data} />
       <div className={styles.footer}>
         <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
